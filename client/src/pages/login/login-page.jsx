40a2fe07ec72206183f14451a2230a03bc5fbe71
@Alnkle621 <kaiquemendesilva82@gmail.com>
2023-02-22 21:31:44 -0300

    style: create style from form

--- client/src/pages/login/login-page.jsx
+++ client/src/pages/login/login-page.jsx
@@ -1,41 +1,42 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import *as yup from "yup";
-import { Container } from "./style/styles.componets";
+import * as yup from "yup";
+import { Container, Main, Form, FormProvider } from "./style/styles.componets";
 
 export function LoginPage() {
-  const schame = yup.object({
-    login: yup
-      .string()
-      .required("Email is required")
-      .email("Invalid email address"),
-    password: yup
-      .string()
-      .required("Password is required")
-      .min(6, "Password must be at least 6 characters"),
-  });
-  const { register, handleSubmit, errors } = useForm({
-      resolver: yupResolver(schame),
+  const validate = yup
+    .object({
+      email: yup.string().required("Email is required"),
+      password: yup.string().required("Password is required"),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validate),
   });
   const onSubmit = (data) => console.log(data);
 
   return (
-    <section>
-      <Container>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div>
-            <label>login</label>
-            <input {...register("login")} placeholder="login" />
-            <p>{errors.login?.message}</p>
-          </div>
-          <div>
-            <label>password</label>
+    <Container>
+      <Main>
+        <h2>LOGIN</h2>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <FormProvider>
+            <label>e-mail</label>
+            <input {...register("login")} placeholder="e-mail" />
+            <p style={{ color: "red" }}>{errors.email?.message}</p>
+            <label>Password</label>
             <input {...register("password")} placeholder="password" />
-            <p>{errors.password?.message}</p>
-          </div>
-        </form>
-      </Container>
-    </section>
+            <p style={{ color: "red" }}>{errors.password?.message}</p>
+            <button>login</button>
+          </FormProvider>
+        </Form>
+      </Main>
+    </Container>
   );
 }
--- client/src/pages/login/style/styles.componets.js
+++ client/src/pages/login/style/styles.componets.js
@@ -1,10 +1,40 @@
 import styled from "styled-components";
 
 const Container = styled.section`
-  width: 200px;
-  height: 200px;
+  width: 100%;
+  height: 100vh;
   background-color: blue;
-  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export { Container };
+const Main = styled.main`
+  width: 700px;
+  height: 500px;
+  background-color: white;
+
+  display: grid;
+  grid-template-columns: 50% 50%;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  height: auto;
+  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const FormProvider = styled.div`
+  width: 80%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: black;
+`;
+
+export { Container, Main, Form, FormProvider };
